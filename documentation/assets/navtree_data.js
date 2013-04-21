56b2152371a0cd6c124a301dec6b3226bccb007c
moagrius <dunn74@gmail.com>
2013-04-20 22:29:38 -0500

    new docs

--- documentation/assets/navtree_data.js
+++
@@ -1,8 +0,0 @@
-var NAVTREE_DATA =
-[ [ "com.qozix.mapview", "com/qozix/mapview/package-summary.html", [ [ "Interfaces", null, [ [ "MapView.MapEventListener", "com/qozix/mapview/MapView.MapEventListener.html", null, "" ] ]
-, "" ], [ "Classes", null, [ [ "MapView", "com/qozix/mapview/MapView.html", null, "" ] ]
-, "" ] ]
-, "" ] ]
-
-;
-
--- documentation/reference/lists.js
+++
@@ -1,6 +0,0 @@
-var DATA = [
-      { id:0, label:"com.qozix.mapview", link:"com/qozix/mapview/package-summary.html", type:"package" },
-      { id:1, label:"com.qozix.mapview.MapView", link:"com/qozix/mapview/MapView.html", type:"class" },
-      { id:2, label:"com.qozix.mapview.MapView.MapEventListener", link:"com/qozix/mapview/MapView.MapEventListener.html", type:"class" }
-
-    ];
